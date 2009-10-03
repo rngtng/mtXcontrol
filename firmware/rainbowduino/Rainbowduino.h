19f5b95541f664f8aa857d8a822370d25b323abd
TobiTobes <t.bielohlawek@gmail.com>
2009-10-03 17:31:38 +0200

    removed old lines, updated comment

--- firmware/rainbowduino/Rainbowduino.h
+++ firmware/rainbowduino/Rainbowduino.h
@@ -1,5 +1,6 @@
 /*
 RAINBOWDUINO.h - A driver to run Seeedstudio 8x8 RBG LED Matrix
+Based on Rainbow.h by Seedstudio.com -> http://www.seeedstudio.com/depot/rainbowduino-led-driver-platform-plug-and-shine-p-371.html
 Copyright (c) 2009 Tobias Bielohlawek
 
 */
@@ -29,25 +30,13 @@ Copyright (c) 2009 Tobias Bielohlawek
 
 #define shift_data_1     {SH_PORT |= SH_BIT_SDI;}
 #define shift_data_0     {SH_PORT &= ~SH_BIT_SDI;}
-//============================================
-//#define open_line0	{PORTB=0x04;} //B00000100
-//#define open_line1	{PORTB=0x02;} //B00000010
-//#define open_line2	{PORTB=0x01;} //B00000001
-//#define open_line3	{PORTD=0x80;} //B10000000
-//#define open_line4	{PORTD=0x40;} //B01000000
-//#define open_line5	{PORTD=0x20;} //B00100000
-//#define open_line6	{PORTD=0x10;} //B00010000
-//#define open_line7	{PORTD=0x08;} //B00001000
-
-//#define close_all_line	{PORTD&=~0xf8;PORTB&=~0x07;}
-//============================================
 
 class Rainbowduino {
 
 public:
   Rainbowduino();
-  void set_row(byte line, byte level, byte r, byte b, byte g );
-  void set_color(byte c );
+  void set_row(byte line, byte level, byte r, byte b, byte g);
+  void set_color(byte c);
   void open_line(byte line);
 };
 
