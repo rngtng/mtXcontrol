658979008265e6158b780dcff117d75b2f49b176
rngtng <tobi@rngntg.com>
2010-01-06 22:51:55 +0100

    updated deploy script to patch Info.plist and rename appfolder to mtXcontrol

--- update_binaries.sh
+++ update_binaries.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
 
 rm ~/Desktop/latest_mtXcontrol_and_firmware_linux.zip
+# copy firmware
 mkdir application.linux/firmware
 cp ~/Sites/java/rainbowduino/firmware/*.* application.linux/firmware
-zip -x .DS_Store -r ~/Desktop/latest_mtXcontrol_and_firmware_linux.zip application.linux/
+#rename
+mv application.linux mtXcontrol
+zip -x .DS_Store -r ~/Desktop/latest_mtXcontrol_and_firmware_linux.zip mtXcontrol/
+rm -rf mtXcontrol
 
+#only 32 Bit version by now
 rm ~/Desktop/latest_mtXcontrol_and_firmware_macosx.zip
-mkdir application.macosx/firmware
+#patch Mac Os X file to use java 1.6
+mv application.macosx/mtXcontrol.app/Contents/Info.plist application.macosx/mtXcontrol.app/Contents/Info_old.plist
+sed 's/1\.5/1\.6/g' application.macosx/mtXcontrol.app/Contents/Info_old.plist > application.macosx/mtXcontrol.app/Contents/Info.plist
+#copy icon
 cp -f sketch.icns application.macosx/mtXcontrol.app/Contents/Resources/sketch.icns
+# copy firmware
+mkdir application.macosx/firmware
 cp ~/Sites/java/rainbowduino/firmware/*.* application.macosx/firmware
-zip -x .DS_Store -r ~/Desktop/latest_mtXcontrol_and_firmware_macosx.zip application.macosx/
+#rename
+mv application.macosx mtXcontrol
+zip -x .DS_Store -r ~/Desktop/latest_mtXcontrol_and_firmware_macosx.zip mtXcontrol/
+rm -rf mtXcontrol
 
 rm ~/Desktop/latest_mtXcontrol_and_firmware_windows.zip
+# copy firmware
 mkdir application.windows/firmware
 cp ~/Sites/java/rainbowduino/firmware/*.* application.windows/firmware
-zip -x .DS_Store -r ~/Desktop/latest_mtXcontrol_and_firmware_windows.zip application.windows/
-
-rm -rf application.*
+#rename
+mv application.windows mtXcontrol
+zip -x .DS_Store -r ~/Desktop/latest_mtXcontrol_and_firmware_windows.zip mtXcontrol/
+rm -rf mtXcontrol
